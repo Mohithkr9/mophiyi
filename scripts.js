b7fa78dc893ac62f371eb37a4cb0c9210b4b7602
Mohithkr9 <mohithmohith228@gmail.com>
2026-07-02 17:51:03 +0530

    bike website

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function bookBike(){
+
+alert("Thank you for choosing Bike World!\nOur sales team will contact you soon.");
+
+}
+
+function contact(){
+
+let name = prompt("Enter Your Name");
+
+if(name){
+
+alert("Thank you " + name + ". We will contact you shortly.");
+
+}
+
+}
